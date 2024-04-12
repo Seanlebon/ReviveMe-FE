825a0f0f39b6afaacfe9dc016deda920e0f64482
Shashank Anand <anandkannan.shashank62@gmail.com>
2024-04-12 01:11:44 -0400

    Add thread sort dropdown to home page

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -5,6 +5,7 @@ import axios from '../../apis/reviveme';
 import { Thread } from '../../types/CommonTypes';
 import ThreadCard from '../../components/ThreadCard/ThreadCard';
 import useAxios from '../../hooks/useAxios';
+import { Dropdown } from 'react-bootstrap';
 
 const Home: React.FC = () => {
   const [threads, error, loading] = useAxios({
@@ -12,6 +13,8 @@ const Home: React.FC = () => {
     method: 'GET',
     url: '/api/v1/threads',
   });
+
+  const [sort, setSort] = React.useState<string | null>('newest');
 
   return (
     <div>
@@ -22,6 +25,57 @@ const Home: React.FC = () => {
       {!loading && !error && threads && (
         <div className='container'>
           <h2>Home Page</h2>
+
+          <div
+            className='card'
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'flex-end',
+              alignItems: 'center',
+              alignContent: 'center',
+              border: 'none',
+              cursor: 'default',
+            }}
+          >
+            <p style={{ margin: 0 }}>Sort by: </p>
+            <Dropdown
+              data-bs-theme='dark'
+              onSelect={(eventKey: string | null, event: Object) => {
+                setSort(eventKey);
+              }}
+            >
+              <Dropdown.Toggle
+                id='dropdown-button-dark-sort-menu'
+                variant='secondary'
+                style={{
+                  backgroundColor: 'transparent',
+                  color: 'black',
+                  border: 'none',
+                  width: '6em', // This is to make it so the 'sort by' text doesn't move when the sort changes
+                }}
+              >
+                {sort}
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  href='#/action-1'
+                  active={sort === 'newest'}
+                  eventKey={'newest'}
+                >
+                  Newest
+                </Dropdown.Item>
+                <Dropdown.Item
+                  href='#/action-2'
+                  active={sort === 'top'}
+                  eventKey={'top'}
+                >
+                  Top
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
           {threads.map((thread: Thread) => (
             <div key={thread.id}>
               <ThreadCard thread={thread} />
